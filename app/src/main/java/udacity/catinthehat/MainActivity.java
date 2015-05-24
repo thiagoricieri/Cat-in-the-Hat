56974e63dd8011c8bb5df50030febb89f7870c2f
Thiago Ricieri <thiago.ricieri@gmail.com>
2015-05-24 15:25:05 -0300

    Buttons events and toasts

--- app/src/main/java/udacity/catinthehat/MainActivity.java
+++ app/src/main/java/udacity/catinthehat/MainActivity.java
@@ -2,8 +2,11 @@ package udacity.catinthehat;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -12,27 +15,55 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		ButterKnife.inject(this);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu_main, menu);
-		return true;
+	@OnClick(R.id.btn_the_cat)
+	public void onTheCatClick(View v){
+		Toast.makeText(this,
+		    getString(R.string.the_cat),
+		    Toast.LENGTH_LONG).show();
 	}
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
+	@OnClick(R.id.btn_thing1)
+	public void onThing1Click(View v){
+		Toast.makeText(this,
+			getString(R.string.thing1),
+			Toast.LENGTH_LONG).show();
+	}
 
-		//noinspection SimplifiableIfStatement
-		if (id == R.id.action_settings) {
-			return true;
-		}
+	@OnClick(R.id.btn_thing2)
+	public void onThing2Click(View v){
+		Toast.makeText(this,
+			getString(R.string.thing2),
+			Toast.LENGTH_LONG).show();
+	}
 
-		return super.onOptionsItemSelected(item);
+	@OnClick(R.id.btn_sally)
+	public void onSallyClick(View v){
+		Toast.makeText(this,
+			getString(R.string.sally),
+			Toast.LENGTH_LONG).show();
+	}
+
+	@OnClick(R.id.btn_nick)
+	public void onNickClick(View v){
+		Toast.makeText(this,
+			getString(R.string.nick),
+			Toast.LENGTH_LONG).show();
+	}
+
+	@OnClick(R.id.btn_thingj)
+	public void onThingjClick(View v){
+		Toast.makeText(this,
+			getString(R.string.thingamajigger),
+			Toast.LENGTH_LONG).show();
+	}
+
+	@OnClick(R.id.btn_seuss)
+	public void onSeussClick(View v){
+		Toast.makeText(this,
+			getString(R.string.dr_seuss),
+			Toast.LENGTH_LONG).show();
 	}
 }
